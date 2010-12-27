ad4cb14b8f459fe41bc06431f31f901c6ffada8c
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-12-27 05:04:15 +0000

    moving products and variations to their rightfull place....

---
+++ _config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * @author Nicolaas modules [at] sunnysideup.co.nz
+ *
+ **/
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START ecommerce_products MODULE ----------------===================
+
+//MUST SET
+//Object::add_extension("ProductVariation", "EcommerceItemDecorator");
+
+
+//MAY SET
+//ProductsAndGroupsModelAdmin::set_managed_models(array(("Product", "ProductGroup","ProductVariation"));
+//===================---------------- END ecommerce_products MODULE ----------------===================
