440aa30e8a4c76c1c317301e096f85570b63cc7b
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-12-28 07:03:50 +0000

    _config.php cleanup

--- _config.php
+++ _config.php
@@ -1,20 +1,14 @@
 <?php
 
 
-/**
- * @author Nicolaas modules [at] sunnysideup.co.nz
- *
- **/
-
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_products MODULE ----------------===================
-//MUST SET
+Object::add_extension("ProductVariation", "Buyable");
 Object::add_extension("Product", "ProductWithVariationDecorator");
 Object::add_extension("Product_Controller", "ProductWithVariationDecorator_Controller");
-Product_Controller::$allowed_actions[] = 'VariationForm';
-Product_Controller::$allowed_actions[] = 'addVariation';
+Product_Controller::$allowed_actions[] = 'variationform';
+Product_Controller::$allowed_actions[] = 'addvariation';
 
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+// __________________________________ START ECOMMERCE PRODUCT VARIATIONS MODULE CONFIG __________________________________
 //MAY SET
 //ProductsAndGroupsModelAdmin::set_managed_models(array(("Product", "ProductGroup","ProductVariation"));
-//===================---------------- END ecommerce_products MODULE ----------------===================
+// __________________________________ END ECOMMERCE PRODUCT VARIATIONS MODULE CONFIG __________________________________
