b3caf59971e995980f9dcc062563d754640daea4
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-12-30 14:10:35 +0000

    starting to add CreateEcommerceVariations idea...

--- code/control/CreateEcommerceVariations.php
+++ code/control/CreateEcommerceVariations.php
@@ -9,6 +9,7 @@ class CreateEcommerceVariations extends Controller {
 		"select",
 		"rename",
 		"add",
+		"delete",
 		"move",
 	);
 
@@ -17,17 +18,19 @@ class CreateEcommerceVariations extends Controller {
 	protected $_id = 0;
 	protected $_valueID = 0;
 	protected $_name = "";
-	protected $_position = "";
+	protected $_position = -1; //use -1 to distinguish it from 0 (first in sorting order)
+	protected $_select = -1;//use -1 to distinguish it from 0 (not selected)
 
 	function init() {
 		parent::init();
-		if(isset($_GET["typeorvalue"])) { $this->_typeorvalue = intval($_GET["_typeorvalue"]);}
-		if(isset($_GET["id"])) { $this->_id = intval($_GET["_id"]);}
-		if(isset($_GET["name"])) { $this->_name = intval($_GET["name"]);}
-		if(isset($_GET["position"])) { $this->_position = intval($_GET["_position"]);}
 		if(!Permission::check("CMS_ACCESS_CMSMain")) {
-			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
+			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need CMS rights to access it. Enter your credentials below and we will send you right along.'));
 		}
+		if(isset($_GET["typeorvalue"])) { $this->_typeorvalue = $_GET["_typeorvalue"];}
+		if(isset($_GET["id"])) { $this->_id = intval($_GET["_id"]);}
+		if(isset($_GET["name"])) { $this->_name = urldecode($_GET["name"]);}
+		if(isset($_GET["position"])) { $this->_position = intval($_GET["_position"]);}
+		if(isset($_GET["select"])) { $this->_select = intval($_GET["select"]);}
 	}
 
 	function createvariations() {
@@ -44,14 +47,16 @@ class CreateEcommerceVariations extends Controller {
 			foreach($typeDos as $typeDo) {
 				$jsonTypeStringForArray = '{';
 				$typeDo->IsSelected = "to be coded";
+				$typeDo->CanDeleteType = $typeDo->canDelete();
 				$valueDos = $typeDo->Values();
-				$jsonTypeStringForArray .= '"TypeID": "'.$typeDo->ID.'", "TypeName": "'.$typeDo->Name.'", "TypeIsSelected": "'.$typeDo->IsSelected.'"';
+				$jsonTypeStringForArray .= '"TypeID": "'.$typeDo->ID.'", "TypeName": "'.$typeDo->Name.'", "TypeIsSelected": "'.$typeDo->IsSelected.'", "CanDeleteType": "'.$typeDo->CanDeleteType.'"';
 				if($valueDos) {
 					$jsonTypeStringForArray .= ', "ValueSize": '.$valueDos->count().', "ValueItems": [';
 					foreach($valueDos as $valueDo) {
 						$jsonValueStringForArray = '{';
 						$valueDo->IsSelected = "to be coded";
-						$jsonValueStringForArray .= '"ValueID": "'.$valueDo->ID.'", "ValueName": "'.$valueDo->Value.'", "ValueIsSelected": "'.$valueDo->IsSelected.'"';
+						$valueDo->CanDeleteValue = $valueDo->canDelete();
+						$jsonValueStringForArray .= '"ValueID": "'.$valueDo->ID.'", "ValueName": "'.$valueDo->Value.'", "ValueIsSelected": "'.$valueDo->IsSelected.'", "CanDeleteValue": "'.$valueDo->CanDeleteValue.'"';
 						$jsonValueStringForArray .= '}';
 						$jsonValueArray[] = $jsonValueStringForArray;
 					}
@@ -68,18 +73,35 @@ class CreateEcommerceVariations extends Controller {
 	}
 
 	function select() {
+		//is it Type or Value?
+		//is it select or unselect
+		//save value
 		die("not completed yet");
 		return "ok";
 	}
 	function rename() {
+		//is it Type or Value?
+		//save new name
 		die("not completed yet");
 		return jsonforform();
 	}
 	function add() {
+		//is it Type or Value?
+		//can the item be added
+		// add item
+		die("not completed yet");
+		return jsonforform();
+	}
+	function delete() {
+		//is it Type or Value?
+		//can the item be deleted
+		// delete item if it is allowed
 		die("not completed yet");
 		return jsonforform();
 	}
 	function move() {
+		//is it Type or Value?
+		//move Item
 		die("not completed yet");
 		return "ok";
 	}
