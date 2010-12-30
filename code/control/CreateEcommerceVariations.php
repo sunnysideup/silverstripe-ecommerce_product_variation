0ef82bb5de3a9edef794515dd393406c9f0a25e9
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-12-30 07:20:25 +0000

    starting to add CreateEcommerceVariations idea...
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_variation/trunk@538 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/control/CreateEcommerceVariations.php
@@ -0,0 +1,65 @@
+<?php
+
+
+class CreateEcommerceVariations extends Controller {
+
+	static $allowed_actions = array(
+		"jsonforform",
+		"createvariations",
+		"selectattribute",
+		"selectattributevalue",
+		"addattribute",
+		"addattributevalue",
+		"moveattribute",
+		"moveattributevalue"
+
+	);
+
+	function init() {
+		parent::init();
+		if(!Permission::check("CMS_ACCESS_CMSMain")) {
+			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
+		}
+	}
+
+	function createvariations() {
+		die("not completed yet");
+	}
+	function jsonforform() {
+		//create dataobjectset here...
+		$dos = ;
+		$fields = array();
+		$dataFormatter = new convertDataObjectSet();
+		echo $dataFormatter->convertDataObjectSet($dos, $fields = null);
+	}
+	function selectattribute() {
+		die("not completed yet");
+		return "ok";
+	}
+	function selectattributevalue() {
+		die("not completed yet");
+		return "ok";
+	}
+	function addattribute() {
+		die("not completed yet");
+		return jsonforform();
+	}
+	function addattributevalue() {
+		die("not completed yet");
+		return jsonforform();
+	}
+	function moveattribute() {
+		die("not completed yet");
+		return "ok";
+	}
+	function moveattributevalue() {
+		die("not completed yet");
+		return "ok";
+	}
+
+}
+
+
+class CreateEcommerceVariations_Field extends FormField() {
+
+}
