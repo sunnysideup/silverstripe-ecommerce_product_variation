8abc9c5496bc3ff4129777962a845c23c48a95e3
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-05-10 01:01:47 +0000

    API: introducing BestAvailableImage method
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_variation/trunk@2617 950b781c-c544-efab-2f99-90083ee69683

--- code/model/ProductVariation.php
+++ code/model/ProductVariation.php
@@ -415,6 +415,27 @@ class ProductVariation extends DataObject implements BuyableModel{
 
 
 	//IMAGES
+	/**
+	 * returns a "BestAvailable" image if the current one is not available
+	 * In some cases this is appropriate and in some cases this is not.
+	 * For example, consider the following setup
+	 * - product A with three variations
+	 * - Product A has an image, but the variations have no images
+	 * With this scenario, you want to show ONLY the product image
+	 * on the product page, but if one of the variations is added to the
+	 * cart, then you want to show the product image.
+	 * This can be achieved bu using the BestAvailable image.
+	 * @return Image | Null
+	 */
+	public function BestAvailableImage() {
+		$image = $this->Image();
+		if($image && $image->exists()) {
+			return $image;
+		}
+		if($product = $this->Product()) {
+			return $product->BestAvailableImage();
+		}
+	}
 
 	/**
 	 * Returns a link to a default image.
