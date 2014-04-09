afab507774e791831312ae157f4b589449312585
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 04:39:36 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_variation/trunk@4924 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -1,16 +1,6 @@
 <?php
 
 
-
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-
-// __________________________________ START ECOMMERCE PRODUCT VARIATIONS MODULE CONFIG __________________________________
-
-//____________CUSTOMISED CMS INTERACTION
-//LeftAndMain::require_javascript("mysite/javascript/MyCreateEcommerceVariationsField.js");
-
-// ____________ IMPORTANT NAME CHANGES....
-//
-
-// __________________________________ END ECOMMERCE PRODUCT VARIATIONS MODULE CONFIG __________________________________
+/**
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+ */
